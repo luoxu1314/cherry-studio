121988398e2212c55f3d6154568f06775b349b5c
luoxu1314 <32211456+luoxu1314@users.noreply.github.com>
2025-07-15 22:08:11 +0800

    Update IpcChannel.ts

--- packages/shared/IpcChannel.ts
+++ packages/shared/IpcChannel.ts
@@ -63,7 +63,6 @@ export enum IpcChannel {
   Mcp_AddServer = 'mcp:add-server',
   Mcp_RemoveServer = 'mcp:remove-server',
   Mcp_RestartServer = 'mcp:restart-server',
-  Mcp_RefreshServer = 'mcp:refresh-server',
   Mcp_StopServer = 'mcp:stop-server',
   Mcp_ListTools = 'mcp:list-tools',
   Mcp_CallTool = 'mcp:call-tool',
